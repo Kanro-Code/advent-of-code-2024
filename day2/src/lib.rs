65859061163d9ab4214b571041e944bd49813b36
Kanro-Code <tervelde.thijs@gmail.com>
2025-03-17 03:28:12 +0100

    Created structure for day 2, preparing for part 2

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -2,6 +2,39 @@ use std::path::PathBuf;
 
 pub const INPUT: &str = "./src/input.txt";
 
+pub struct Report {
+    pub list: Vec<u8>,
+}
+
+impl Report {
+    pub fn new(list: Vec<u8>) -> Self {
+        Self { list }
+    }
+
+    pub fn is_sorted(&self) -> bool {
+        self.list.is_sorted()
+    }
+
+    pub fn is_sorted_dec(&self) -> bool {
+        self.list.is_sorted_by(|a, b| a >= b)
+    }
+
+    pub fn has_valid_diff(&self) -> bool {
+        self.list.windows(2).all(|window| {
+            if let &[a, b] = window {
+                let diff = a.abs_diff(b);
+                diff > 0 && diff <= 3
+            } else {
+                false
+            }
+        })
+    }
+
+    pub fn is_valid_sequence(&self) -> bool {
+        (self.is_sorted() || self.is_sorted_dec()) && self.has_valid_diff()
+    }
+}
+
 pub fn load_file(path: impl Into<PathBuf>) -> String {
     std::fs::read_to_string(path.into()).expect("Failed to read file")
 }
@@ -18,26 +51,10 @@ pub fn split_file(file: String) -> Vec<Vec<u8>> {
     holder
 }
 
-pub fn is_sorted_desc_or_asc<T>(vec: &[T]) -> bool
-where
-    T: PartialOrd,
-{
-    vec.is_sorted() || vec.is_sorted_by(|a, b| a >= b)
-}
-
-pub fn has_valid_diff(vec: &[u8]) -> bool {
-    vec.windows(2).all(|window| {
-        if let &[a, b] = window {
-            let diff = a.abs_diff(b);
-            diff > 0 && diff <= 3
-        } else {
-            false
-        }
-    })
-}
-
-pub fn is_valid_sequence(vec: &[u8]) -> bool {
-	is_sorted_desc_or_asc(vec) && has_valid_diff(vec)
+pub fn load_and_create_reports(path: impl Into<PathBuf>) -> Vec<Report> {
+    let file_content = load_file(path);
+    let lines = split_file(file_content);
+    lines.into_iter().map(Report::new).collect()
 }
 
 #[cfg(test)]
@@ -46,13 +63,8 @@ mod parts {
 
     #[test]
     fn part1() {
-        let file = load_file(INPUT);
-        let lines = split_file(file);
-
-        let total = lines
-            .iter()
-            .filter(|e| is_valid_sequence(e))
-            .count();
+        let reports = load_and_create_reports(INPUT);
+        let total = reports.iter().filter(|e| e.is_valid_sequence()).count();
 
         println!("Solution for day 2, part 1 is: {total}.")
     }
