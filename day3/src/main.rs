c420d384b65b19c9a7f097897dee120a1bfca071
Kanro-Code <tervelde.thijs@gmail.com>
2025-03-18 03:42:44 +0100

    Day 3, part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,12 +2,14 @@ use lazy_regex::regex_captures_iter;
 
 fn main() {
     let file = load_file("./src/input.txt");
-	let mut lines = Vec::new();
-    for line in regex_captures_iter!(r"(mul\(\d{0,3},\d{0,3}\))", &file) {
-		lines.push(line.get(1).unwrap().as_str());
+	let mut lines: Vec<u64> = Vec::new();
+    for line in regex_captures_iter!(r"mul\((\d{0,3}),(\d{0,3})\)", &file) {
+		let (a, b) = (line.get(1).unwrap().as_str().parse::<u64>().unwrap(), line.get(2).unwrap().as_str().parse::<u64>().unwrap());
+		lines.push(a * b);
     }
 
-	println!("{:#?}", lines);
+	let total: u64 = lines.iter().sum();
+	println!("Day 3 Part 1: {}", total);
 }
 
 pub fn load_file(file_path: &str) -> String {
